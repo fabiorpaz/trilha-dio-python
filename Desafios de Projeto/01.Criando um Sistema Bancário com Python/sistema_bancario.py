1761e7c4c2c057d0bc2c8ef931111de3fe570f08
Fabio <fabioreis.paz@gmail.com>
2023-01-22 12:22:47 -0300

    adicionando projeto

--- Desafios de Projeto/01.Criando um Sistema Bancário com Python/sistema_bancario.py
+++ Desafios de Projeto/01.Criando um Sistema Bancário com Python/sistema_bancario.py
@@ -18,13 +18,42 @@ while True:
     opcao = input(menu)
     
     if opcao == 'd':
-        print('Depósito')    
+        valor = float(input("Por favor, informe qual o valor a ser depositado: "))
+        if valor > 0:
+            saldo+=valor
+            extrato += f"Depósito: R${valor:.2f}\n"
+        else:
+            print ("Operação de depósito inválida")
+
     elif opcao == 's':
-        print('Saque')
+        print ("Por favor, informe o valor do saque: ")
+        
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print ("Operação inválida, você não possui saldo suficiente")
+        elif excedeu_limite:
+            print ("Operação inválida, você não possui limite suficiente")
+        elif excedeu_saques:
+            print ("Operação inválida, você excedeu o limite de saques")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R${valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print ("Operação inválida")
+
     elif opcao =='e':
-        print('Extrato')
+        print("\n========== EXTRATO ==========")
+        print("Não foram realizadas movimetações na conta." if not extrato else extrato)
+        print(f"\nSaldo: R${saldo:.2f}")
+        print("=============================")
+
     elif opcao == 'q':
         break
+
     else:
         print('Operação inválida, por favor selecione novamente a operação desejada.')
     
